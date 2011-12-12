74b16d197e0707ee2bf69754f5f8893a73b08214
alaingilbert <alain.gilbert.15@gmail.com>
2011-12-12 08:42:13 -0500

    Use expressjs framework

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http       = require('http')
   , fs         = require('fs')
+  , app        = require('express').createServer()
   , zmq        = require('zmq')
   , PORT       = process.argv[2] || 8080
   , HOST       = process.argv[3] || '127.0.0.1'
@@ -11,37 +12,37 @@ sock.connect(ZMQ_STRING);
 sock.subscribe('room');
 
 
-http.createServer(function (req, res) {
-   console.log(new Date().toTimeString()+' -- '+req.method+' '+req.url);
+app.get('/events', function (req, res) {
+   res.writeHead(200, { 'Content-Type'  : 'text/event-stream'
+                      , 'Cache-Control' : 'no-cache'
+                      , 'Connection'    : 'keep-alive'
+                      });
+   console.log('Client connect');
 
-   if (req.url == '/events') {
-      res.writeHead(200, { 'Content-Type'  : 'text/event-stream'
-                         , 'Cache-Control' : 'no-cache'
-                         , 'Connection'    : 'keep-alive'
-                         });
-      console.log('Client connect');
+   var callback = function (data) {
+      console.log('Send data');
+      res.write('data: '+data.toString()+'\n\n');
+   };
 
-      var callback = function (data) {
-         console.log('Send data');
-         res.write('data: '+data.toString()+'\n\n');
-      };
+   sock.on('message', callback);
 
-      sock.on('message', callback);
+   res.socket.on('close', function () {
+      sock.removeListener('message', callback);
+      console.log('Client leave');
+   });
+});
 
-      res.socket.on('close', function () {
-         sock.removeListener('message', callback);
-         console.log('Client leave');
-      });
 
-   } else if (req.url == '/') {
-      fs.readFile(__dirname+'/templates/index.html', function (err, file) {
-         res.writeHead(200, {'Content-Type': 'text/html'});
-         res.write(file, 'binary');
-         res.end()
-      });
-   }
-}).listen(PORT, HOST);
+app.get('/', function (req, res) {
+   fs.readFile(__dirname+'/templates/index.html', function (err, file) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(file, 'binary');
+      res.end()
+   });
+});
 
+
+app.listen(PORT, HOST);
 
 console.log('Server listening on '+HOST+':'+PORT);
 console.log(new Date().toTimeString());
